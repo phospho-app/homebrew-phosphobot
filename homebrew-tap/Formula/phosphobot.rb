48614b97f7a6699c467a2caa40c7dd4ddeba084d
Frédéric Legrand <frederic@phospho.ai>
2025-02-12 13:58:55 +0100

    Create phosphobot.rb

---
+++ homebrew-tap/Formula/phosphobot.rb
@@ -0,0 +1,15 @@
+class Phosphobot < Formula
+    desc "Phosphobot teleop application for robot control"
+    homepage "https://github.com/phospho-app/phosphobot"
+    url "https://github.com/phospho-app/homebrew-phosphobot/releases/download/0.2.45/phosphobot.bin"
+    sha256 "57fbde7418a3c89ba409a735f79ed8e616774bfeda2e5320fcd67d66b91d68ad"
+    license "MIT"
+  
+    def install
+      bin.install "phosphobot"
+    end
+  
+    test do
+      system "#{bin}/phosphobot", "--version"
+    end
+  end
