1d62e199041df72f9d32d2c865c4ce97953edb02
Frédéric Legrand <frederic@phospho.ai>
2025-02-12 16:14:49 +0100

    Update phosphobot.rb

--- Formula/phosphobot.rb
+++ Formula/phosphobot.rb
@@ -1,7 +1,7 @@
 class Phosphobot < Formula
   desc "Phosphobot teleop application for robot control"
   homepage "https://github.com/phospho-app/phosphobot"
-  url "https://github.com/phospho-app/phospho-app/homebrew-phosphobot/releases/download/0.2.49/phosphobot.bin"
+  url "https://github.com/phospho-app/homebrew-phosphobot/releases/download/0.2.49/phosphobot.bin"
   sha256 "ccf3aa2460edb0b31232825735f62c52dd38f2b353f27d001abd40af213287c6"
   license "MIT"
 
