24230a17473eff9e9ea720355ccdb68aab592148
GitHub Action <action@github.com>
2025-02-18 17:30:12 +0000

    Update phosphobot to version 0.2.55

--- Formula/phosphobot.rb
+++ Formula/phosphobot.rb
@@ -1,8 +1,8 @@
 class Phosphobot < Formula
   desc "Phosphobot teleop application for robot control"
   homepage "https://github.com/phospho-app/phosphobot"
-  url "https://github.com/phospho-app/homebrew-phosphobot/releases/download/0.2.54/phosphobot.bin"
-  sha256 "2104b81ad860f972a7fb02db921fc18016961797e099f0e56f20eeaea9a8bcc0"
+  url "https://github.com/phospho-app/homebrew-phosphobot/releases/download/0.2.55/phosphobot.bin"
+  sha256 "2a92d45cf260c00d0b2c20e5cc69a2774238ac1e8485379f8a67f4e991ac5920"
   license "MIT"
 
   def install
