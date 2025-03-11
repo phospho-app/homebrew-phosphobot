a2fbaee23f2a4bb310c512e09aae3248c895e386
GitHub Action <action@github.com>
2025-03-11 15:40:18 +0000

    Update phosphobot to version 0.2.83 with multi-arch support

--- Formula/phosphobot.rb
+++ Formula/phosphobot.rb
@@ -5,11 +5,11 @@ class Phosphobot < Formula
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/phospho-app/homebrew-phosphobot/releases/download/0.2.82/phosphobot-arm64.bin"
-      sha256 "ee92e2a39997ef23973d2e3468ba54c5a834b4ab3bbbc7775e4d982e54892a99"
+      url "https://github.com/phospho-app/homebrew-phosphobot/releases/download/0.2.83/phosphobot-arm64.bin"
+      sha256 "80d3038af360a90a15ab463c0c6bb28da31d6b6046ae791105abe5459d612268"
     else
-      url "https://github.com/phospho-app/homebrew-phosphobot/releases/download/0.2.82/phosphobot-x86_64.bin"
-      sha256 "25cddcf65b3b9e44edc40be9e226c463ce6a6863964b91b6522d7488aa70c972"
+      url "https://github.com/phospho-app/homebrew-phosphobot/releases/download/0.2.83/phosphobot-x86_64.bin"
+      sha256 "8badc7360787d83cff0e6963fab367c4e055a55c2ee98d92b39f938ecc31bd1e"
     end
   end
   
