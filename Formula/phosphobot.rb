d7e16316b03a4fd02b9fc39b46803b59547545f9
Nicolas Oulianov <58109554+oulianov@users.noreply.github.com>
2025-03-24 21:01:24 +0100

    Update phosphobot.rb

--- Formula/phosphobot.rb
+++ Formula/phosphobot.rb
@@ -6,10 +6,10 @@ class Phosphobot < Formula
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/phospho-app/homebrew-phosphobot/releases/download/0.2.99/phosphobot-0.2.99-arm64.bin"
+      url "https://github.com/phospho-app/homebrew-phosphobot/releases/download/v0.2.99/phosphobot-0.2.99-arm64.bin"
       sha256 "11afe9bc354370a51f9cd610854d23838ca12543bcb18e09487c5e7e18e81838"
     else
-      url "https://github.com/phospho-app/homebrew-phosphobot/releases/download/0.2.99/phosphobot-0.2.99-x86_64.bin"
+      url "https://github.com/phospho-app/homebrew-phosphobot/releases/download/v0.2.99/phosphobot-0.2.99-x86_64.bin"
       sha256 "18218af04ca83d7dee6ce99f70ceffb1c916ca49efa19f59394b7f46a125f86a"
     end
   end
