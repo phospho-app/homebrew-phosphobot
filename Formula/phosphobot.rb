559bb71826beeed95902d990b7798f68724d49cb
GitHub Action <action@github.com>
2025-04-04 17:39:32 +0000

    Update phosphobot to version 0.2.112 with multi-arch support

--- Formula/phosphobot.rb
+++ Formula/phosphobot.rb
@@ -2,21 +2,21 @@ class Phosphobot < Formula
   desc "Phosphobot teleop application for robot control"
   homepage "https://github.com/phospho-app/phosphobot"
   license "MIT"
-  version "0.2.111"
+  version "0.2.112"
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/phospho-app/homebrew-phosphobot/releases/download/v0.2.111/phosphobot-0.2.111-arm64.bin"
-      sha256 "f87b38c9c9a16bd135314de1e931417c54ac5562b9e2cf05527bec41e6be249c"
+      url "https://github.com/phospho-app/homebrew-phosphobot/releases/download/v0.2.112/phosphobot-0.2.112-arm64.bin"
+      sha256 "ce49376e9d29952697c6ce6cebf6493abd0f03313a37217b5725ff8be1a0b259"
     else
-      url "https://github.com/phospho-app/homebrew-phosphobot/releases/download/v0.2.111/phosphobot-0.2.111-x86_64.bin"
-      sha256 "71c7095139da1763f84fa659bab9449fa1a0477d0b2347dc0bb77af532678935"
+      url "https://github.com/phospho-app/homebrew-phosphobot/releases/download/v0.2.112/phosphobot-0.2.112-x86_64.bin"
+      sha256 "a879f53f2c2548a3a9ca3370d0c993b409776556f7534969982a95184484bf24"
     end
   end
   
   def install
     arch = Hardware::CPU.arm? ? "arm64" : "x86_64"
-    bin_name = "phosphobot-0.2.111-#{arch}.bin"
+    bin_name = "phosphobot-0.2.112-#{arch}.bin"
     chmod "+x", bin_name
     bin.install bin_name => "phosphobot"
   end
